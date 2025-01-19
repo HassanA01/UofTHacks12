f83eda66623388e8cad73ba21ff9e377434bebed
HassanA01 <aneeq.hassan@utoronto.ca>
2025-01-18 21:54:26 -0500

    updated backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,9 +12,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+// app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
